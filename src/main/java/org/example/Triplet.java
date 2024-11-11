2f2ebf295a5d05b716c73e565df7950d50d7eb24
Z4RCO <z3rc0mrm@gmail.com>
2024-11-11 23:52:15 +0100

    Added Triplet class

---
+++ src/main/java/org/example/Triplet.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Triplet {
+    public Circle c1, c2, c3;
+
+    public Triplet(Circle c1, Circle c2, Circle c3) {
+        this.c1 = c1;
+        this.c2 = c2;
+        this.c3 = c3;
+    }
+}
